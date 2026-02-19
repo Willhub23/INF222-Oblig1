1061e81bb7d4620d9dce2c39c0026160e7a3fb2b
William Lloyd <wlloyd02@hotmail.com>
2026-02-19 18:29:09 +0100

    final solution

--- src/main/java/inf222/aop/account/aspect/TransferAspect.java
+++ src/main/java/inf222/aop/account/aspect/TransferAspect.java
@@ -1,44 +1,111 @@
 package inf222.aop.account.aspect;
 
-import java.lang.reflect.Method;
-
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
+
 
 import inf222.aop.account.Account;
 import inf222.aop.account.annotation.Transfer;
 
 /* TODO: annotation */
+@Aspect
 public class TransferAspect {
 
     /* TODO: add the join point & pointcut & advice */
-    public ................................................ {
+    @Around("execution(* *(..)) && @annotation(transfer)")
+    public Object logTransfer(ProceedingJoinPoint pjp, Transfer transfer) throws Throwable {
         /* TODO */
+        Logger logger = LoggerFactory.getLogger(pjp.getTarget().getClass());
 
-        Logger logger = LoggerFactory.getLogger(......................);
+        Object[] args = pjp.getArgs();
+        Double amount = (Double) args[2];
 
-        /* TODO */
-        logger.atLevel(.............).log(.......................);
+        if (transfer.internationalTransfer()) {
+            String msg = logInternationalTransfer(args);
+            logger.atLevel(transfer.value()).log(msg);
+        }
 
-        /* TODO */
+        if (amount != null && amount > transfer.LogTransferAbove()) {
+            String msg = logTransferAbove(args, transfer.LogTransferAbove());
+            logger.atLevel(transfer.value()).log(msg);
+        }
+
+        try {
+            Object result = pjp.proceed();
+            if (transfer.logErrors() && result instanceof Boolean && !((Boolean) result)) {
+                MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+                String methodName = methodSignature.getName();
+                String[] paramNames = methodSignature.getParameterNames();
+
+                logger.atLevel(transfer.value()).log(logErrors(args, methodName, paramNames));
+            }
+            return result;
+
+        } catch (Throwable t) {
+            if (transfer.logErrors()) {
+                MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+                String methodName = methodSignature.getName();
+                String[] paramNames = methodSignature.getParameterNames();
+
+                logger.atLevel(transfer.value()).log(logErrors(args, methodName, paramNames));
+            }
+            throw t;
+        }
+
     }
 
+
     private String logInternationalTransfer(Object[] methodArgs) {
+        Account from = (Account) methodArgs[0];
+        Account to = (Account) methodArgs[1];
+        Double amount = (Double) methodArgs[2];
         /* TODO */
-        var message = String.format("International transfer from %s to %s, %s %s converted to %s", /* TODO */);
+        var message = String.format("International transfer from %s to %s, %s %s converted to %s",
+                from.getAccountName(),
+                to.getAccountName(),
+                amount,
+                from.getCurrency(),
+                to.getCurrency());
+
         return message;
     }
 
     private String logTransferAbove(Object[] methodArgs, double value) {
         /* TODO */
-        var message = String.format("Transfer above %s from %s to %s, amount: %s", /* TODO */);
+        Account from = (Account) methodArgs[0];
+        Account to = (Account) methodArgs[1];
+        Double amount = (Double) methodArgs[2];
+
+        var message = String.format("Transfer above %s from %s to %s, amount: %s",
+                value,
+                from.getAccountName(),
+                to.getAccountName(),
+                amount);
         return message;
     }
 
     private String logErrors(Object[] methodArgs, String methodName, String[] methodParams) {
         /* TODO */
-        var message = String.format("Error in transfer from %s to %s, amount: %s %s, method: %s(%s)", /* TODO */);
+        Account from = (Account) methodArgs[0];
+        Account to = (Account) methodArgs[1];
+        Double amount = (Double) methodArgs[2];
+
+        String params = (methodParams == null || methodParams.length == 0)
+                ? "from, to, amount"
+                : String.join(", ", methodParams);
+
+        var message = String.format("Error in transfer from %s to %s, amount: %s %s, method: %s(%s)",
+                from.getAccountName(),
+                to.getAccountName(),
+                amount,
+                from.getCurrency(),
+                methodName,
+                params);
+
         return message;
     }
 }
--- src/main/java/inf222/aop/measures/MeasureAspect.java
+++ src/main/java/inf222/aop/measures/MeasureAspect.java
@@ -1,11 +1,19 @@
 package inf222.aop.measures;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /* TODO: annotation */
+
+// Alle klasser som inneholder advices må være merket med @Aspect
+@Aspect()
 public class MeasureAspect {
     private final String regex;
     private final Pattern pattern;
@@ -19,10 +27,50 @@ public class MeasureAspect {
 
     public MeasureAspect() {
         String elems = String.join("|", toMeter.keySet());
-        regex = String.format(/* TOOD: Fill in regex */, elems);
+        regex = String.format(".*_(" + elems + ")$");
         pattern = Pattern.compile(regex);
     }
 
-    // TODO other methods
+    // Må bruke Object her for @Around
+
+    @Around("get(double inf222.aop.measures..*)")
+    public Object convertToMeters(ProceedingJoinPoint pjp) throws Throwable {
+        Object value = pjp.proceed();
+
+        String name = pjp.getSignature().getName();
+
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()) {
+            String unit = matcher.group(1);
+
+            double converted = (Double) value * toMeter.get(unit);
+            return converted;
+        }
+        return value;
+
+    }
+
+
+    @Around("set(double inf222.aop.measures..*) && !cflow(execution(inf222.aop.measures.Measures.new(..)))")
+    public Object convertBackToOriginal(ProceedingJoinPoint pjp) throws Throwable {
+        String name = pjp.getSignature().getName();
+
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()) {
+            String unit = matcher.group(1);
+
+            Object inValueMeters = pjp.getArgs()[0];
+
+            double originalValue = (Double) inValueMeters / toMeter.get(unit);
+
+            if (originalValue < 0)
+                throw new Error("Illegal modification");
+
+            return pjp.proceed(new Object[]{originalValue});
+        }
+
+        return pjp.proceed();
+
+    }
 
 }
